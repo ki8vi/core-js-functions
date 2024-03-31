e1a768ff4416e119398762ac08cc5fd79179ded5
ki8vi <oneofbln@gmail.com>
2024-03-31 14:21:27 +0600

    feat: solve getIdGeneratorFunction task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -251,8 +251,17 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+
+function getIdGeneratorFunction(startFrom) {
+  const objData = {
+    callTimes: startFrom,
+    showValue: function addOne() {
+      this.lastVal = this.callTimes;
+      this.callTimes += 1;
+      return this.lastVal;
+    },
+  };
+  return objData.showValue.bind(objData);
 }
 
 module.exports = {
